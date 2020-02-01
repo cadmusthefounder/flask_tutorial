eda1e0def3d8faf70d0363e2d703461b8d20df2e
Charlton Lim <cadmusthefounder@gmail.com>
2020-02-01 15:52:57 +0800

    feat(root): add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'type-enum': [2, 'always', [
+        'build',
+        'ci',
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ]
+    ],
+    'scope-enum': [2, 'always', [
+        'root',
+        'server',
+        'model'
+      ]
+    ]
+  }
+};
